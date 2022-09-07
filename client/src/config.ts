237a9b52e2660180046509a33e526493da8afc68
Joshua Ehoche <103928526+Adventuresbyjosh@users.noreply.github.com>
2022-09-07 19:21:17 +0100

    upload files

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'laysuzu645';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev--u2-03pt.us.auth0.com',
+  clientId: 'IABV3q2GZJShmeXx2XTDyUzKgnp9xdNR',
+  callbackUrl: 'http://localhost:3000/callback'
+};
